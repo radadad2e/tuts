71a4369daa57c923219528f02dba73be993d6881
Rick Foster <115846221+rick-bt@users.noreply.github.com>
2023-11-01 09:46:05 -0700

    Update Directory.java

--- java/patterns/visitor/src/us/fosteronline/FSOObjects/Directory.java
+++ java/patterns/visitor/src/us/fosteronline/FSOObjects/Directory.java
@@ -1,6 +1,7 @@
 package us.fosteronline.FSOObjects;
 import us.fosteronline.FSOVisitors.*;
 
+// commentary
 public class Directory implements FSOInterface {
   @Override public int accept(FSOVisitorInterface visitor) {
     return visitor.visit(this);
